6ca0c4254a31f0dc0d62c98742c79ed9c9877e4d
jumpy-jumper <42717910+jumpy-jumper@users.noreply.github.com>
2021-09-23 22:14:06 -0400

    Fix the follower panels for real

--- Stage/UI/follower_panel.gd
+++ Stage/UI/follower_panel.gd
@@ -29,26 +29,23 @@ func update_panel():
 	
 	$Border.modulate = selected_color if stage.selected_follower_index == int(name)-1 else deselected_color
 	
-	var unit = get_cur_unit()
-	if not unit:
-		$CooldownPanel.visible = true
-		$Level.text = ""
-		$Cost.text = ""
-		return
+	var first_unit = follower_group.values()[0]
+	var cur_unit = get_cur_unit()
 	
-	if unit.get_alive_in_group():
+	if first_unit.get_alive_in_group():
 		$CooldownPanel.visible = true
 		$CooldownPanel.modulate.a = base_alpha
-		$CooldownPanel/Cooldown.text = unit.Wind.keys()[unit.get_alive_in_group().wind][0]
+		$CooldownPanel/Cooldown.text = first_unit.Wind.keys()[first_unit.get_alive_in_group().wind][0]
 	else:
-		$Level.text = "L" + str(unit.get_stat("level", unit.base_level))
-		$Cost.text = str(unit.get_stat("cost", unit.base_cost))
-		$Cost.modulate = Color.lightpink if unit.summoner.faith < unit.get_stat("cost", unit.base_cost) else Color.white
-		if unit.cooldown > 0:
+		if cur_unit:
+			$Level.text = "L" + str(cur_unit.get_stat("level", cur_unit.base_level))
+			$Cost.text = str(cur_unit.get_stat("cost", cur_unit.base_cost))
+			$Cost.modulate = Color.lightpink if cur_unit.summoner.faith < cur_unit.get_stat("cost", cur_unit.base_cost) else Color.white
+		if first_unit.cooldown > 0:
 			$CooldownPanel.visible = true
 			$CooldownPanel.modulate.a = base_alpha
-			$CooldownPanel/Cooldown.text = str(unit.cooldown)
-		elif unit.get_stat("cost", unit.base_cost) <= unit.summoner.faith:
+			$CooldownPanel/Cooldown.text = str(first_unit.cooldown)
+		elif cur_unit and cur_unit.get_stat("cost", cur_unit.base_cost) <= cur_unit.summoner.faith:
 			$CooldownPanel.visible = false
 			$CooldownPanel/Cooldown.text = ""
 		else:
@@ -60,8 +57,10 @@ func update_panel():
 func _on_Follower_Panel_gui_input(event):
 	if event is InputEventMouseButton and not event.pressed:
 		if stage.can_select_follower_ui():
-			stage.selected_follower_index = int(name)-1
-			if event.button_index == BUTTON_RIGHT:
+			if event.button_index == BUTTON_LEFT:
+				stage.selected_follower_index = int(name)-1
+			elif event.button_index == BUTTON_RIGHT:
+				stage.selected_follower_index = int(name)-1
 				if stage.can_show_ui():
 					yield(get_tree(), "idle_frame")
 					yield(get_tree(), "idle_frame")
